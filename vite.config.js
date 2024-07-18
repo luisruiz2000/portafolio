1780ded19582ca60b30089bdabaea9065fe0d4d6
luisruiz2000 <luisruiz462000@gmail.com>
2024-07-18 10:56:06 -0500

    Solucion bug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://luisruiz2000.github.io/portafolio'
+  base: 'https://luisruiz2000.github.io/portafolio'
 })
